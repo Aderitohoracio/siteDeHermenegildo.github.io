8ecc5b0a1d2ba36f5a6308c2e5554715fc3f380f
Aderitohoracio <aderitohoracio41@gmail.com>
2023-10-05 20:06:41 +0200

    ultimo

---
+++ baixar.js
@@ -0,0 +1,5 @@
+function openWhatsap () {
+  const baixar = './PDF.html'
+  window.open(baixar)
+}
+document.getElementById('baixar').addEventListener('click',openWhatsap)
---
+++ script.js
@@ -0,0 +1,5 @@
+function openWhatsap () {
+  const linkWhatsap = 'https://chat.whatsapp.com/EVVi4LwCXOt7aGyYZbY0Aw'
+  window.open(linkWhatsap)
+}
+document.getElementById('enterWhatsap').addEventListener('click',openWhatsap)

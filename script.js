43298d3af990ea9e8737dcf14aed29a7081a8ec0
Aderitohoracio <aderitohoracio41@gmail.com>
2023-10-05 20:34:21 +0200

    outro

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function openWhatsap () {
-  const linkWhatsap = 'https://chat.whatsapp.com/EVVi4LwCXOt7aGyYZbY0Aw'
+  const linkWhatsap = 'https://chat.whatsapp.com/KuwPtu0OIPM0HGZbkl9fac'
   window.open(linkWhatsap)
 }
 document.getElementById('enterWhatsap').addEventListener('click',openWhatsap)
